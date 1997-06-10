bd9e077d2f5735a0b6b2e2c0263298f4603f31bf
wes <beepage@umich.edu>
1997-06-10 23:54:50 +0000

    added code to keep server's pid in a file, and an option to reload the server

--- daemon.c
+++ daemon.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+#include <sys/file.h>
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -92,6 +93,10 @@ main( ac, av )
     struct servent	*se;
     int			c, s, err = 0, fd, sinlen;
     int			dontrun = 0;
+    int			restart = 0;
+    int			pidfd;
+    FILE		*pf;
+    char		*pidfile = _PATH_PIDFILE;
     char		*prog;
     unsigned short	port = 0;
     extern int		optind;
@@ -103,8 +108,12 @@ main( ac, av )
 	prog++;
     }
 
-    while (( c = getopt( ac, av, "cdp:b:M:" )) != -1 ) {
+    while (( c = getopt( ac, av, "rcdp:b:M:" )) != -1 ) {
 	switch ( c ) {
+	case 'r' :		/* restart server */
+	    restart++;
+	    break;
+
 	case 'c' :		/* check config files */
 	    dontrun++;
 	    break;
@@ -144,6 +153,36 @@ main( ac, av )
     if ( chdir( _PATH_SPOOL ) < 0 ) {
 	perror( _PATH_SPOOL );
 	exit( 1 );
+    }
+
+    if ( restart ) {
+	int		pid;
+
+	/* open pid file */
+	if (( pidfd = open( pidfile, O_RDONLY, 0 )) < 0 ) {
+	    perror( pidfile );
+	    exit( 1 );
+	}
+	/* make sure we *can't* get a lock */
+	if ( flock( pidfd, LOCK_EX | LOCK_NB ) == 0 || errno != EWOULDBLOCK ) {
+	    fprintf( stderr, "%s: no daemon running!\n", prog );
+	    exit( 1 );
+	}
+
+	if (( pf = fdopen( pidfd, "r" )) == NULL ) {
+	    fprintf( stderr, "%s: can't fdopen pidfd!\n" );
+	    exit( 1 );
+	}
+	fscanf( pf, "%d\n", &pid );
+	if ( pid <= 0 ) {
+	    fprintf( stderr, "%s: bad pid %d!\n", pid );
+	    exit( 1 );
+	}
+	if ( kill( pid, SIGHUP ) < 0 ) {
+	    perror( "kill" );
+	    exit( 1 );
+	}
+	exit( 0 );
     }
 
     if ( dontrun ) {
@@ -194,6 +233,24 @@ main( ac, av )
 	exit( 1 );
     }
 
+    /* open and truncate the pid file */
+    if (( pidfd = open( pidfile, O_CREAT | O_WRONLY, 0644 )) < 0 ) {
+	perror( pidfile );
+	exit( 1 );
+    }
+    if ( flock( pidfd, LOCK_EX | LOCK_NB ) < 0 ) {
+	if ( errno == EWOULDBLOCK ) {
+	    fprintf( stderr, "%s: already running!\n", prog );
+	} else {
+	    perror( "flock" );
+	}
+	exit( 1 );
+    }
+    if ( ftruncate( pidfd, 0 ) < 0 ) {
+	perror( "ftruncate" );
+	exit( 1 );
+    }
+
     /*
      * Disassociate from controlling tty.
      */
@@ -204,7 +261,7 @@ main( ac, av )
 	case 0 :
 	    dt = getdtablesize();
 	    for ( i = 0; i < dt; i++ ) {
-		if ( i != s ) {			/* keep listen socket open */
+		if ( i != s && i != pidfd ) {	/* keep socket and pidfd open */
 		    (void)close( i );
 		}
 	    }
@@ -234,6 +291,13 @@ main( ac, av )
 #else ultrix
     openlog( prog, LOG_NOWAIT|LOG_PID, LOG_TPPD );
 #endif ultrix
+
+    if (( pf = fdopen( pidfd, "w" )) == NULL ) {
+	syslog( LOG_ERR, "can't fdopen pidfd" );
+	exit( 1 );
+    }
+    fprintf( pf, "%d\n", getpid());
+    fflush( pf );	/* leave pf open, since it's flock-ed */
 
     /* catch SIGHUP */
     bzero( &sa, sizeof( struct sigaction ));
