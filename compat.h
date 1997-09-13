ed425d35f7970dfd22b7a6f8b724bf3039e11446
wes <beepage@umich.edu>
1997-09-13 16:30:23 +0000

    Initial revision

---
+++ compat.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 1997 Regents of The University of Michigan.
+ * All Rights Reserved.  See COPYRIGHT.
+ */
+
+# if defined( sun ) && defined( __svr4__ )
+
+#define LOCK_SH		(1<<0)
+#define LOCK_EX		(1<<1)
+#define LOCK_NB		(1<<2)
+#define LOCK_UN		(1<<4)
+
+# endif sun __svr4__
