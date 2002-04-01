a52a6b3ca131fffc8b5b2f9611afd89547c114f3
messina <beepage@umich.edu>
2002-04-01 22:28:22 +0000

    Headers for rfc2045.c

---
+++ rfc2045.h
@@ -0,0 +1,12 @@
+/*
+* Copyright (c) 2002 Regents of The University of Michigan.
+* All Rights Reserved.  See COPYRIGHT.
+*/
+
+#ifdef __STDC__
+#define ___P(x)         x
+#else __STDC__
+#define ___P(x)         ()
+#endif __STDC__
+
+int	parse_content_type ___P(( char **, char *, char **, char **, char **, char **, int * ));
