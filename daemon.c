65a413398d64a8b78380cfc2c7ca97460d716235
wes <beepage@umich.edu>
2002-12-24 04:52:11 +0000

    sometimes accept returns bogus errors

--- daemon.c
+++ daemon.c
@@ -438,11 +438,10 @@ main( ac, av )
 
 	sinlen = sizeof( struct sockaddr_in );
 	if (( fd = accept( s, (struct sockaddr *)&sin, &sinlen )) < 0 ) {
-	    if ( errno == EINTR ) {
-		continue;
+	    if ( errno != EINTR ) {
+		syslog( LOG_ERR, "accept: %m" );
 	    }
-	    syslog( LOG_ERR, "accept: %m" );
-	    exit( 1 );
+	    continue;
 	}
 
 	/* start child */
